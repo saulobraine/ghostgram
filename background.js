22b90db928e59e21b43ffab77509b76532cbe47c
Saulo Braine <saulobraine@outlook.com>
2026-02-08 13:44:03 -0300

    refactor: remover scripts obsoletos e limpar estrutura do projeto

--- background.js
+++
@@ -1,5 +0,0 @@
-// Background service worker (MV3) - ES module
-import { BackgroundService } from './src/background/BackgroundService.js';
-
-const service = new BackgroundService();
-service.initialize();
