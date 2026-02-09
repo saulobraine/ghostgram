9b289e413154a62f2adf483608d0d0b6fc46ed03
Saulo Braine <saulobraine@outlook.com>
2026-02-09 10:39:29 -0300

    feat(shared): finalize package with exports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -7,3 +7,5 @@ export * from './types/index.js';
 export * from './constants/index.js';
 export * from './dto/index.js';
 export * from './domain/index.js';
+export * from './i18n/index.js';
+export * from './utils/index.js';
